865cf8107f3b00e0f64def7e52577dc3455b38a7
LeonardoGalan <leonardogalan2@gmail.com>
2022-01-16 16:54:01 -0500

    added skeleton for functions needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cellColor: ""
+      rows: 1,
+      columns: 1
+    }
+  }
+  
+
+  
+  
+ 
+addRow = () => {
+  this.setState({rows: this.state.rows + 1});
 }
 
+  
+addColumn = () =>{
+  this.setState({columns: this.state.rows + 1});
+}
+
+selectColor = () => {
+  
+}
+  
+render () {
+  return (
+    <div>
+      <div></div>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+       </div>
+    
+  )
+}
+
+
+
+
+
+
+
+
+  
+
+
+
+  
+}
 export default App;
